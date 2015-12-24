74547d7f87f6fe607cecb6dd62376a12855325ae
dparmar16 <parmar.d@gmail.com>
2015-12-24 01:33:57 -0800

    Create drive id number to analyze drives.

--- CreateDriveNumber.R
+++ CreateDriveNumber.R
@@ -1,6 +1,9 @@
+#Data already loaded in NFLPlay.R
+
 #Generate drive numbers based on the data
 install.packages("ggplot2")
 library(ggplot2)
+library(sqldf)
 
 #First look at the data already loaded
 head(nfldata)
@@ -25,6 +28,7 @@ for (x in 1:(nrow(nfldata))) {
 
 #Look at drivenumber vector
 drivenumber[1:25]
+nfldata[1:25,]
 
 #Get counts data frame
 drive_tab <- rle(sort(drivenumber))
@@ -33,4 +37,15 @@ head(drive_table)
 
 #hist(drive_table)
 qplot(drive_table$count,data=drive_table, geom="histogram", binwidth = 1, 
-      xlab="Plays in Drive",ylab="Number of Drives")
+      xlab="Plays in Drive",ylab="Number of Drives",main = "Number of Drives with Amount of Plays ")
+
+#Make sure drive number lines up
+cbind(nfldata[1:50,6],c(1,drivenumber[1:49]))
+dim(nfldata)
+nfl_updated <- cbind(nfldata,c(1,drivenumber)) 
+names(nfl_updated)[31] <- "driveid"
+head(nfl_updated)
+
+#Do some spot checking to double check numbers match up
+sqldf("select driveid from nfl_updated group by driveid having count(driveid) = 4 limit 5")
+sqldf("select gid, pid, off,def,type,dseq,len,dwn,fd,driveid from nfl_updated where driveid in (10,11,12,36,37,38)")
